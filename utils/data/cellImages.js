bd11bbbcbaffeae8e670ff794e90916090fba3e2
ecy <ecyfemi@stanford.edu>
2024-02-20 21:39:13 -0800

    notes added

--- utils/data/cellImages.js
+++ utils/data/cellImages.js
@@ -5,9 +5,9 @@ import Image3 from '@/data/cellImages/stanford.jpeg'
 import Image4 from '@/data/cellImages/finished_draft.jpeg'
 import CenterImage from '@/data/cellImages/bit_center.jpeg'
 import Image5 from '@/data/cellImages/copies.jpeg'
-import Image6 from '@/data/cellImages/presentations.jpeg'
+import Image6 from '@/data/cellImages/design_ideas.jpeg'
 
-import Image7 from '@/data/cellImages/features.jpeg'
+import Image7 from '@/data/cellImages/present.jpeg'
 import Image8 from '@/data/cellImages/garnet.jpeg'
 
 // images for the main, cellView stuff
